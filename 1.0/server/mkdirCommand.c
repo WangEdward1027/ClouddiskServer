f13032bd7ff8ff860ab362a7ebc476994161e399
RickSanchez <8378411+SherryJerez@user.noreply.gitee.com>
2024-06-17 16:26:33 +0800

    LSQ: MKDIR.c

---
+++ 1.0/server/mkdirCommand.c
@@ -0,0 +1,36 @@
+/*
+ *@author LSQ created
+ *
+ *
+ *
+ */
+
+#include "thread_pool.h"
+#include <stdio.h>
+#include <string.h>
+#include <sys/syslog.h>
+
+#define MODE 0777
+
+void mkdirCommand(task_t * task){
+    
+    char *message1="Create dir Sucessful!";
+    char *message2="Create dir Failed!";
+    printf("Execute mkdir command.");
+    
+    train_t t;
+    memset(&t,0,sizeof(t));//初始化
+
+    if(mkdir( task->data , MODE ) ==-1){
+        strcpy(t.buff,message2);
+        t.len=strlen(t.buff);
+        sendn(task->peerfd,&t,t.len+4);
+        syslog(LOG_WARNING,"User execute MKDIR command: Failed.");
+    } 
+    else{
+        strcpy(t.buff,message1);
+        t.len=strlen(t.buff);
+        sendn(task->peerfd,&t,t.len+4);
+        syslog(LOG_INFO,"User execute MKDIR command: Successful.");
+    }
+}
