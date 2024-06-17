c4290fa256f05ebe2d6ad923601c1daf49113794
zjzhang <z468345257@163.com>
2024-06-17 16:37:38 +0800

    张子健 修改pwd.c

--- 1.0/server/pwd.c
+++ 1.0/server/pwd.c
@@ -12,12 +12,19 @@ void pwdCommand(task_t * task)
 {
     
     char* cwd;
+    char buff[1024] = {0};
+    bzero(buff, sizeof(buff));
     if ((cwd = getcwd( NULL, 0)) == NULL) {
         error( 1, errno, "getcwd failed");
     }
     
-    puts(cwd);
+    // 将获取的pwd保存到buff中
+    strcpy(buff, cwd);
+
     free(cwd);
+    
+    // 将buff的内容发送给客户端
+    sendn(task->peerfd, buff, sizeof(buff));
 
 }
 
