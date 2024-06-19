36de20f906a78b06caf0e4bd313584c8c68f031a
Edward <948096711@qq.com>
2024-06-19 17:02:33 +0800

    王绪飞：mdir的ui 和注释 优化

--- 1_2/server/mkdirCommand.c
+++ 1_2/server/mkdirCommand.c
@@ -9,30 +9,28 @@ void mkdirCommand(task_t * task){
     char buff[128];
     bzero(buff,sizeof(buff));
 
-    //// 尝试创建目录
+    //尝试创建目录
     if(mkdir(Dir_Name,MODE)==-1){
-        
         // 检查创建失败是否因为目录已存在
         if(errno==EEXIST){
-            sprintf(buff,"Directory  '%s'  already exists.\n",Dir_Name);
+            sprintf(buff,"目录 '%s' 已经存在.\n",Dir_Name);
             sendn(task->peerfd,buff,sizeof(buff));
             return;
         }
 
         //其他创建失败
         else{
-            sprintf(buff,"Create  '%s'  Failed.\n",Dir_Name);
+            sprintf(buff,"创建目录  '%s'  失败.\n",Dir_Name);
             sendn(task->peerfd,buff,sizeof(buff));
-
-            syslog(LOG_WARNING,"User execute MKDIR command: Failed.");
+            syslog(LOG_WARNING,"User execute MKDIR command: Failed."); //日志
             return;
         }
     } 
     else{
-        //创建成功
-        sprintf(buff,"Create  '%s'  Sucessful.",Dir_Name);
+        //目录创建成功
+        sprintf(buff,"创建目录 '%s' 成功.",Dir_Name);
         sendn(task->peerfd,buff,sizeof(buff));
-        syslog(LOG_INFO,"User execute MKDIR command: Successful.");
+        syslog(LOG_INFO,"User execute MKDIR command: Successful."); //日志
         return;
     }
 }
