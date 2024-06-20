df5d5d7a32a5aea2d8b9d6032ac838602cfea324
Edward <948096711@qq.com>
2024-06-20 15:54:47 +0800

    王绪飞：登录模块完全版

--- 3.0/server/login.c
+++ 3.0/server/login.c
@@ -2,32 +2,43 @@
 #include <openssl/rand.h>
 #define SALT_LENGTH 16
 
-//生成盐值的函数在注册 register.c中
-
-
+//用户登录1: 接收用户名，对比用户表该用户是否存在。存在发盐值，不存在不发。
 void userLoginCheck1(task_t * task)
 {
     char username[64];
-    
     sscanf(task->data,"CMD_TYPE_REGISTER_USERNAME:%s",username);
     
-    if(selectUserByUserName(username)){
-        snprintf(task->data,sizeof(task->data),"USER_EXISTS");
-    }else{
-        char salt[SALT_LENGTH*2+1];
-               
-        generateSalt(salt,SALT_LENGTH);
-        
-        strncpy(task->user->userName,username,sizeof(task->user->userName));
-
-        snprintf(task->data,sizeof(task->data),"SALT:%s",salt);
-        //将盐值保存以便后续使用
-        strncpy(task->user->salt,salt,sizeof(task->user->salt));
+    //若用户名存在,发送该用户对应的盐值
+    User* user = selectUserByUserName(username);
+    if(user != NULL){
+        strncpy(task->user->salt, user->salt, sizeof(task->user->salt));
+    }
+    //若用户名不存在,返回用户名不存在,不发送盐值
+    else{
+        snprintf(task->data,sizeof(task->data),"用户名不存在");
     }
     send(task->peerfd,task->data,strlen(task->data),0);
 }
 
+//用户登录2:验证密码
 void userLoginCheck2(task_t * task)
 {
+    //接收密码
+    char encrypted_password[64];
+    sscanf(task->data,"CMD_TYPE_ENCRYPTECODE:%s",encrypted_password);
 
+    //去数据库中获取密码,进行对比
+        //准备操作:保存用户名，待会用用户名查询用户表
+        char username[64];
+        sscanf(task->data,"CMD_TYPE_REGISTER_USERNAME:%s",username);
+        User* user = selectUserByUserName(username);
+    //若密码相同，则返回登录成功
+    if(encrypted_password == user->cryptpasswd){
+        snprintf(task->data,sizeof(task->data),"MSG_TYPE_LOGINOK");
+    }
+    //若密码不同，则返回登录失败
+    else{
+        snprintf(task->data,sizeof(task->data),"MSG_TYPE_LOGINERROR");
+    }
+    send(task->peerfd,task->data,strlen(task->data),0);
 }
