f69aa4eef45b220a7324aab4aa05674c191844bb
zjzhang <z468345257@163.com>
2024-06-20 15:57:49 +0800

    张子健 未测试的pwd.c

--- 3.0/server/pwd.c
+++ 3.0/server/pwd.c
@@ -2,5 +2,13 @@
 
 void pwdCommand(task_t * task)
 {
-
+    char* path;
+    char buff[512];
+    path = task->user->pwd;
+    
+    strcpy(buff, path);
+    strcat(buff, "\n");
+    sendn(task->peerfd, buff, strlen(buff));
+    
+    return;
 }
