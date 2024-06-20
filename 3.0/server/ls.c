33dcc77f4543717d73535cc74bd9ac0646d7e351
zjzhang <z468345257@163.com>
2024-06-20 15:43:04 +0800

    未测试的ls.c cd.c

--- 3.0/server/ls.c
+++ 3.0/server/ls.c
@@ -44,53 +44,6 @@ char* getParentDirectory(const char* pwd) {
     return NULL;
 }
 
-
-bool do_select(task_t* task, char* sql) {
-    
-    char buff[1024] = {0};
-    char filename[20] = {0};
-    // 1. 创建数据库连接
-    MYSQL* mysql = create_db_connection();
-
-    // 2. 查询数据库
-    int qret = mysql_query(mysql, sql);
-    if (qret != 0) {
-        // 查询失败，退出
-        strcpy(buff, "输入无效的路径!!!\n");
-        sendn(task->peerfd, buff, strlen(buff));
-        return false;
-    }
-
-    // 3. 获取查询结果
-    MYSQL_RES* res = mysql_store_result(mysql);
-    if (res == NULL) {
-        // 查询失败，退出
-        strcpy(buff, "获取结果出错!!!\n");
-        sendn(task->peerfd, buff, strlen(buff));
-        return false;
-    }
-
-    // 4. 访问结果集
-    MYSQL_ROW row;
-    while ((row = mysql_fetch_row(res)) != NULL) {
-        sprintf(filename, "%-15s\t", row[0]);
-        strcat(buff, filename);
-    }
-    
-    // 5. 返回查询结果
-    sendn(task->peerfd, buff, strlen(buff));
-
-    // 6. 释放结果集
-    mysql_free_result(res);
-
-    // 关闭数据库连接
-    mysql_close(mysql);
-
-    return true;
-}
-
-
-
 void lsCommand(task_t * task)
 {
     int parent_id; // 保存用户当前父目录id
@@ -101,9 +54,9 @@ void lsCommand(task_t * task)
     if (strcmp(task->data, "") == 0) {
         // 当前ls后面无参数
         filename = getCurrentDirectory(task->user->pwd);
-    } else if (strcmp(task->data, "./") == 0){
+    } else if (strcmp(task->data, "./") == 0 || strcmp(task->data, ".") == 0){
         filename = getCurrentDirectory(task->user->pwd); 
-    } else if (strcmp(task->user->pwd, "../") == 0) {
+    } else if (strcmp(task->user->pwd, "../") == 0 || strcmp(task->data, "..") == 0) {
         filename = getParentDirectory(task->user->pwd);
         if (filename == NULL) {
             strcpy(buff, "无效命令，重新输入。\n");
