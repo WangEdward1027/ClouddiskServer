39f2864372e83031ebb7728be303dcc7ed4b5e4c
Edward <948096711@qq.com>
2024-06-21 15:32:25 +0800

    王绪飞：补login.c的第二段小火车

--- 3.0/client/login.c
+++ 3.0/client/login.c
@@ -13,7 +13,7 @@ int login_client(int sockfd, User* user){
     char encrypted_password[BUFFER_SIZE];
     
     printf("请输入用户名：\n");
-    fgets(username, sizeof(username), stdin);
+    scanf("%s",username);
     username[strcspn(username, "\n")] = 0;
     //登录第一次交互，只填充User结构体的用户名
     strncpy(user->userName, username, sizeof(user->userName)); //2填充用户名
@@ -22,12 +22,14 @@ int login_client(int sockfd, User* user){
     //填充小火车协议
     train_t t;
     memset(&t, 0, sizeof(t));
-    t.len = strlen(username);
+    t.len = strlen(request);
     t.type = CMD_TYPE_LOGIN_USRNAME;
     t.user = *user;
     strcpy(t.buff, request);
     //发送小火车
-    send(sockfd, &t, 4 + 4 + sizeof(t.user) + t.len, 0);
+    send(sockfd, &t, 4 + 4 + sizeof(User) + t.len, 0);
+    printf("测试：sizeof(t.user):%ld\n", sizeof(t.user));
+    printf("测试：sizeof(User):%ld\n", sizeof(User));
 
     // 接收服务器响应
     receive_response(sockfd, response);
@@ -47,7 +49,14 @@ int login_client(int sockfd, User* user){
         // 发送加密后的密码
         snprintf(request, sizeof(request), "CMD_TYPE_ENCRYPTECODE:%s", encrypted_password);
         strcpy(user->cryptpasswd, encrypted_password);  // 填充加密密码
-        send(sockfd, request,strlen(request),0);
+        
+        //填充小火车协议
+        train_t t;
+        t.len = strlen(request);
+        t.type = CMD_TYPE_LOGIN_ENCRYTPTEDCODE;
+        t.user = *user;
+        strcpy(t.buff, request);
+        send(sockfd, &t, 4 + 4 + sizeof(User) + t.len, 0);
 
         // 接收服务器最终响应
         receive_response(sockfd, response);
