f0c1360eb58d7560ab610a58bc498e14ac66d6d0
小白 <1050811623@qq.com>
2024-06-21 13:51:58 +0800

    张航美FileEntry

--- 3.0/server/FileEntry.c
+++ 3.0/server/FileEntry.c
@@ -20,22 +20,7 @@ int addFileEntry(FileEntry* fileEntry) {
     }
     mysql_free_result(res);
     
-    // 验证md5是否存在
-    snprintf(query, sizeof(query), "SELECT md5 FROM fileinfo WHERE md5 = '%s'", fileEntry->md5);
-    if (mysql_query(conn, query)) {
-        fprintf(stderr, "addFileEntry() failed: %s\n", mysql_error(conn));
-        mysql_close(conn);
-        return -1;
-    }
-    res = mysql_store_result(conn);
-    if (res == NULL || mysql_num_rows(res) == 0) {
-        fprintf(stderr, "addFileEntry() failed: md5 does not exist\n");
-        mysql_free_result(res);
-        mysql_close(conn);
-        return -1;
-    }
-    mysql_free_result(res);
-    
+        
     // 插入FileEntry
     snprintf(query, sizeof(query),
              "INSERT INTO fileentry (parentId, fileName, ownerId, md5, fileSize, fileType) VALUES (%d, '%s', %d, '%s', %d, %d)",
@@ -106,23 +91,7 @@ int updateFileEntry(FileEntry* fileEntry) {
         return -1;
     }
     mysql_free_result(res);
-    
-    // 验证md5是否存在
-    snprintf(query, sizeof(query), "SELECT md5 FROM fileinfo WHERE md5 = '%s'", fileEntry->md5);
-    if (mysql_query(conn, query)) {
-        fprintf(stderr, "updateFileEntry() failed: %s\n", mysql_error(conn));
-        mysql_close(conn);
-        return -1;
-    }
-    res = mysql_store_result(conn);
-    if (res == NULL || mysql_num_rows(res) == 0) {
-        fprintf(stderr, "updateFileEntry() failed: md5 does not exist\n");
-        mysql_free_result(res);
-        mysql_close(conn);
-        return -1;
-    }
-    mysql_free_result(res);
-    
+        
     // 更新FileEntry
     snprintf(query, sizeof(query),
              "UPDATE fileentry SET parent_id = %d, filename = '%s', owner_id = %d, md5 = '%s', filesize = %d, type = %d WHERE id = %d",
