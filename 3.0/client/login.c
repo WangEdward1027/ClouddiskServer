53004476ab9de5d9da66152478ff12343af76c97
Edward <948096711@qq.com>
2024-06-21 18:29:25 +0800

    王绪飞：登录模块修改

--- 3.0/client/login.c
+++ 3.0/client/login.c
@@ -14,49 +14,59 @@ int login_client(int sockfd, User* user){
     
     printf("请输入用户名：\n");
     scanf("%s",username);
-    username[strcspn(username, "\n")] = 0;
+
     //登录第一次交互，只填充User结构体的用户名
     strncpy(user->userName, username, sizeof(user->userName)); //2填充用户名
-    snprintf(request, sizeof(request), "CMD_TYPE_REGISTER_USRNAME:%s", username);
+    strcpy(user->salt, "salt");
+    strcpy(user->cryptpasswd, "cryptpasswd");
+    strcpy(user->pwd, "pwd");
+    /* snprintf(request, sizeof(request), "CMD_TYPE_REGISTER_USRNAME:%s", username); */
     
     //填充小火车协议
     train_t t;
     memset(&t, 0, sizeof(t));
-    t.len = strlen(request);
+    t.len = 1;
+    /* t.len = strlen(request); */
     t.type = CMD_TYPE_LOGIN_USRNAME;
     t.user = *user;
-    strcpy(t.buff, request);
+    /* strcpy(t.buff, request); */
     //发送小火车
     send(sockfd, &t, 4 + 4 + sizeof(User) + t.len, 0);
-    printf("测试：sizeof(t.user):%ld\n", sizeof(t.user));
-    printf("测试：sizeof(User):%ld\n", sizeof(User));
 
     // 接收服务器响应
     receive_response(sockfd, response);
+    printf("response = %s\n",response );
 
     // 盐值检查
     if (strstr(response, "SALT")) {
         sscanf(response, "SALT:%s", salt);
-        printf("盐值：%s\n", salt);
+        printf("测试盐值：%s\n", salt);
+        printf("-----------\n");
 
         // 提示用户输入密码
         printf("请输入密码：\n");
-        fgets(password, sizeof(password), stdin);
-        password[strcspn(password, "\n")] = 0; // 去除换行符
+        scanf("%s",password);
+        printf("输入的密码:%s\n",password);
+        /* fgets(password, sizeof(password), stdin); */
+        /* password[strcspn(password, "\n")] = 0; // 去除换行符 */
 
         // 盐值加密
         encrypt_password(password, salt, encrypted_password);
         // 发送加密后的密码
-        snprintf(request, sizeof(request), "CMD_TYPE_ENCRYPTECODE:%s", encrypted_password);
+        /* snprintf(request, sizeof(request), "CMD_TYPE_ENCRYPTECODE:%s", encrypted_password); */
         strcpy(user->cryptpasswd, encrypted_password);  // 填充加密密码
-        
+        printf("测试:encrypted_password:%s\n",encrypted_password);
+        printf("测试: user->cryptpasswd:%s\n",user->cryptpasswd);
         //填充小火车协议
         train_t t;
-        t.len = strlen(request);
+        t.len = 1;
+        /* t.len = strlen(request); */
         t.type = CMD_TYPE_LOGIN_ENCRYTPTEDCODE;
         t.user = *user;
-        strcpy(t.buff, request);
-        send(sockfd, &t, 4 + 4 + sizeof(User) + t.len, 0);
+        /* strcpy(t.buff, request); */
+        int ret = send(sockfd, &t, 4 + 4 + sizeof(User) + t.len, 0);
+        printf("密码send ret = %d\n",ret);
+
 
         // 接收服务器最终响应
         receive_response(sockfd, response);
@@ -67,7 +77,7 @@ int login_client(int sockfd, User* user){
             return 0;
         }
     }else{
-        printf("该用户不存在。\n");
+        printf("client:该用户不存在。\n");
         return 0;
     }
 }
